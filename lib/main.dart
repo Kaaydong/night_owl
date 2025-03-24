16c200fd2d5d20ce4679a7e0b403810b5de7ed1d
Kaaydong <gippy713@gmail.com>
2025-03-23 17:06:37 -0700

    Configured Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:night_owl/greeting_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(MyApp());
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
